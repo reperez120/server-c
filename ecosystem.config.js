b1b77d5aed7998ec909563871ff7a29b304a000d
Rachel Perez <reperez@me.com>
2020-01-03 16:29:29 -0500

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'https://github.com/reperez120/server-c.git',
       path : '/home/ubuntu/server-c',
-      'post-deploy' : 'npm install && pm2 startOrrestart ecosystem.config.js'
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
